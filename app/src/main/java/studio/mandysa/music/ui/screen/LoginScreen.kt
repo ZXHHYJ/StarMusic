caa1ce052d6a285e17907abc6bc830df69322584
ZXHHYJ <957447668@qq.com>
2022-05-11 13:47:56 +0800

    清理垃圾 优化登录ui

--- app/src/main/java/studio/mandysa/music/ui/screen/LoginScreen.kt
+++ app/src/main/java/studio/mandysa/music/ui/screen/LoginScreen.kt
@@ -1,30 +1,57 @@
 package studio.mandysa.music.ui.screen
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import studio.mandysa.music.R
+import studio.mandysa.music.ui.viewmodel.EventViewModel
 
 @Composable
 @Preview
-fun LoginScreen() {
-    Column(modifier = Modifier.fillMaxWidth().height(400.dp)) {
-        Text(text = stringResource(id = R.string.login))
-        var phone by remember {
-            mutableStateOf("")
+fun LoginScreen(event: EventViewModel = viewModel()) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(400.dp)
+    ) {
+        Column {
+            var phone by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+            TextField(
+                value = phone,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                placeholder = { Text(stringResource(R.string.phone)) },
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = {
+                    phone = it
+                })
+            TextField(
+                value = password,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                placeholder = { Text(stringResource(R.string.password)) },
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = {
+                    password = it
+                })
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                elevation = null,
+                onClick = {
+                    event.login(mobilePhone = phone, password = password)
+                }) {
+                Text(stringResource(R.string.login))
+            }
         }
-        var password by remember {
-            mutableStateOf("")
-        }
-        TextField(value = phone, onValueChange = { phone = it })
-        TextField(value = password, onValueChange = { password = it })
     }
 }
--- app/src/main/java/studio/mandysa/music/ui/screen/StartScreen.kt
+++ app/src/main/java/studio/mandysa/music/ui/screen/StartScreen.kt
@@ -1,51 +1,46 @@
 package studio.mandysa.music.ui.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import studio.mandysa.music.R
 import studio.mandysa.music.ui.common.MultiBottomSheet
-import studio.mandysa.music.ui.theme.horizontalMargin
 
 @Composable
 fun StartScreen(openSheet: (MultiBottomSheet.Intent) -> Unit) {
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .background(
-                brush = Brush.linearGradient(
-                    colors = listOf(
-                        Color.Red,
-                        Color.Blue
-                    )
-                ),
-                alpha = 0.5f
-            )
-            .statusBarsPadding()
-            .navigationBarsPadding()
-            .padding(horizontal = horizontalMargin),
+            .fillMaxSize(),
         verticalArrangement = Arrangement.Bottom
     ) {
-        Row(horizontalArrangement = Arrangement.Center) {
-            Text(
-                text = stringResource(R.string.login_to),
-                fontSize = 34.sp,
-                color = Color.White
-            )
-            Text(text = stringResource(R.string.app_name), fontSize = 34.sp)
-        }
-        Button(modifier = Modifier.fillMaxWidth(), onClick = { openSheet.invoke(MultiBottomSheet.Intent()) }) {
-            Text(text = stringResource(id = R.string.login), color = Color.Black)
-        }
-        Button(modifier = Modifier.fillMaxWidth(), onClick = { /*TODO*/ }) {
-            Text(text = stringResource(id = R.string.exit), color = Color.Black)
+        AsyncImage(
+            model = "https://static-ali.ihansen.org/app/bg1440/9b9d6yKkago.jpg!o",
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f)
+        )
+        Row(modifier = Modifier
+            .navigationBarsPadding()) {
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1.0f),
+                onClick = { openSheet.invoke(MultiBottomSheet.Intent()) }) {
+                Text(text = stringResource(id = R.string.login), color = Color.Black)
+            }
+            Button(modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f), onClick = { }) {
+                Text(text = stringResource(id = R.string.exit), color = Color.Black)
+            }
         }
     }
 }
