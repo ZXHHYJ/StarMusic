79523b77146dd893362c76a2c62af77a3b91de99
957447668 <957447668@qq.com>
2022-12-12 18:42:18 +0800

    对songbean序列化处理

--- app/src/main/java/studio/mandysa/music/service/playmanager/bean/SongBean.kt
+++ app/src/main/java/studio/mandysa/music/service/playmanager/bean/SongBean.kt
@@ -4,35 +4,40 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 sealed interface SongBean {
+    @Parcelize
     data class Local(
         val album: Album,
         val artist: Artist,
         val duration: Long,
         val data: String,
         val songName: String
-    ) : SongBean {
+    ) : SongBean, Parcelable {
         @Parcelize
         data class Artist(val id: String, val name: String) : Parcelable
 
-        data class Album(val id: String, val name: String)
+        @Parcelize
+        data class Album(val id: String, val name: String) : Parcelable
     }
 
+    @Parcelize
     open class Network(
         open val id: String,
         open val title: String,
         open val coverUrl: String,
         open val artist: Array<Artist>,
         open val album: Album
-    ) : SongBean {
+    ) : SongBean, Parcelable {
 
-        open class Artist(open val id: String, open val name: String)
+        @Parcelize
+        open class Artist(open val id: String, open val name: String) : Parcelable
 
+        @Parcelize
         open class Album(
             open val id: String,
             open val coverUrl: String,
             open val name: String,
             open val publishTime: String
-        )
+        ) : Parcelable
     }
 
 }
