83723d9d7faeb963db5a4efd24bc2a20d0ee6153
957447668 <957447668@qq.com>
2022-12-13 20:08:04 +0800

    调整了MotionBlur的实现方式

--- app/src/main/java/studio/mandysa/music/ui/common/MotionBlur.kt
+++ app/src/main/java/studio/mandysa/music/ui/common/MotionBlur.kt
@@ -1,28 +1,26 @@
 package studio.mandysa.music.ui.common
 
 import android.annotation.SuppressLint
-import android.webkit.WebView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.viewinterop.AndroidView
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun MotionBlur(modifier: Modifier) {
-    AndroidView(factory = { WebView(it) }, modifier = modifier) {
-        it.settings.javaScriptEnabled = true
-        it.settings.javaScriptCanOpenWindowsAutomatically = false
-        it.settings.useWideViewPort = true
-        it.settings.domStorageEnabled = true
-        it.settings.databaseEnabled = true
-        it.settings.loadWithOverviewMode = false
-        it.settings.loadsImagesAutomatically = true
-        it.settings.defaultTextEncodingName = "utf-8"
-        it.settings.setSupportMultipleWindows(false)
-        it.loadUrl("file:android_asset/motion_blur/index.html")
-    }
+    val state = rememberWebViewState("file:android_asset/motion_blur/index.html")
+    WebView(
+        state = state,
+        onCreated = {
+            it.settings.javaScriptEnabled = true
+            it.settings.domStorageEnabled = true
+        },
+        captureBackPresses = false,
+        modifier = modifier
+    )
 }
 
 @Preview
