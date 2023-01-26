89eef6e7289fde71a95875bdfbed4a8725676ce2
huanghao <957447668@qq.com>
2023-01-26 22:51:53 +0800

    优化了播放界面的显示效果

--- app/src/main/java/studio/mandysa/music/ui/screen/play/NowPlayScreen.kt
+++ app/src/main/java/studio/mandysa/music/ui/screen/play/NowPlayScreen.kt
@@ -34,6 +34,7 @@ import studio.mandysa.music.service.playmanager.ktx.title
 import studio.mandysa.music.ui.common.AppAsyncImage
 import studio.mandysa.music.ui.common.SeekBar
 import studio.mandysa.music.ui.screen.DialogDestination
+import studio.mandysa.music.ui.theme.isAndroidPad
 import studio.mandysa.music.ui.theme.roundedCornerShape
 import studio.mandysa.music.ui.theme.translucentWhite
 import studio.mandysa.music.ui.theme.translucentWhiteFixBug
@@ -46,169 +47,157 @@ fun NowPlayScreen(dialogNavController: NavController<DialogDestination>) {
             .fillMaxSize()
             .systemBarsPadding()
             .padding(horizontal = playScreenHorizontal),
-        verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AlbumCover()
-        TitleAndArtist(dialogNavController)
-        MusicProgressBar()
-        MusicControlBar()
-    }
-}
-
-
-@Composable
-private fun AlbumCover() {
-    var size by remember {
-        mutableStateOf(IntSize.Zero)
-    }
-    Card(
-        modifier = Modifier
-            .padding(bottom = 8.dp)
-            .fillMaxWidth()
-            .height(
-                with(LocalDensity.current) {
-                    size.width.toDp()
-                })
-            .onSizeChanged {
-                size = it
-            },
-        elevation = 10.dp,
-        shape = roundedCornerShape
-    ) {
-        val coverUrl by PlayManager.changeMusicLiveData().map { return@map it.coverUrl }
-            .observeAsState()
-        coverUrl?.let {
-            AppAsyncImage(modifier = Modifier.fillMaxSize(), url = it)
+        if (!isAndroidPad) {
+            Spacer(modifier = Modifier.height(50.dp))
         }
-    }
-}
-
-@Composable
-private fun TitleAndArtist(dialogNavController: NavController<DialogDestination>) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
-    ) {
-        Column(
+        var size by remember {
+            mutableStateOf(IntSize.Zero)
+        }
+        Card(
             modifier = Modifier
-                .weight(1.0f),
-            horizontalAlignment = Alignment.Start
+                .padding(bottom = 8.dp)
+                .fillMaxWidth()
+                .height(
+                    with(LocalDensity.current) {
+                        size.width.toDp()
+                    })
+                .onSizeChanged {
+                    size = it
+                },
+            elevation = 10.dp,
+            shape = roundedCornerShape
         ) {
-            val title by PlayManager.changeMusicLiveData().map { return@map it.title }
-                .observeAsState("")
-            val musician by PlayManager.changeMusicLiveData().map {
-                it.artist[0].name
-            }.observeAsState("")
-            Text(
-                text = title,
-                color = Color.White,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                maxLines = 1
-            )
-            Spacer(modifier = Modifier.padding(top = 2.dp))
-            Text(
-                text = musician,
-                color = translucentWhite,
-                fontSize = 16.sp,
-                maxLines = 1
-            )
+            val coverUrl by PlayManager.changeMusicLiveData().map { return@map it.coverUrl }
+                .observeAsState()
+            coverUrl?.let {
+                AppAsyncImage(modifier = Modifier.fillMaxSize(), url = it)
+            }
         }
-        val song by PlayManager.changeMusicLiveData().observeAsState()
-        Icon(
-            Icons.Rounded.MoreVert, null,
-            Modifier
-                .size(32.dp)
-                .clip(RoundedCornerShape(32.dp))
-                .background(translucentWhiteFixBug)
-                .clickable {
-                    dialogNavController.navigate(DialogDestination.SongMenu(song!!))
-                },
-            tint = Color.White
-        )
-    }
-}
-
-@Composable
-private fun MusicProgressBar() {
-    fun Int.toTime(): String {
-        val s: Int = this / 1000
-        return (s / 60).toString() + ":" + s % 60
-    }
-
-    val musicPlaybackProgress by PlayManager.playingMusicProgressLiveData().map {
-        it
-    }.observeAsState(0)
-    val musicDuration by PlayManager.playingMusicDurationLiveData().map {
-        it
-    }.observeAsState(1)
-
-    SeekBar(
-        modifier = Modifier
-            .fillMaxWidth(),
-        value = musicPlaybackProgress,
-        maxValue = musicDuration,
-        onValueChange = { PlayManager.seekTo(it) }
-    )
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        Text(text = musicPlaybackProgress.toTime(), color = translucentWhite)
-        Spacer(modifier = Modifier.weight(1f))
-        Text(text = musicDuration.toTime(), color = translucentWhite)
-    }
-}
-
-@Composable
-private fun MusicControlBar() {
-    val smallButtonPadding = 10.dp
-    val middleButtonSize = 80.dp
-
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        val playPauseState by PlayManager.pauseLiveData().map {
-            if (it) R.drawable.ic_play else R.drawable.ic_pause
-        }.observeAsState(R.drawable.ic_play)
-        Icon(
-            ImageVector.vectorResource(id = R.drawable.ic_skip_previous), null,
-            Modifier
-                .size(middleButtonSize)
-                .padding(smallButtonPadding)
-                .clip(RoundedCornerShape(middleButtonSize))
-                .clickable {
-                    PlayManager.skipToPrevious()
-                },
-            tint = Color.White
-        )
-        Box(modifier = Modifier.padding(horizontal = 18.dp)) {
+        //-----
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1.0f),
+                horizontalAlignment = Alignment.Start
+            ) {
+                val title by PlayManager.changeMusicLiveData().map { return@map it.title }
+                    .observeAsState("")
+                val musician by PlayManager.changeMusicLiveData().map {
+                    it.artist[0].name
+                }.observeAsState("")
+                Text(
+                    text = title,
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1
+                )
+                Spacer(modifier = Modifier.padding(top = 2.dp))
+                Text(
+                    text = musician,
+                    color = translucentWhite,
+                    fontSize = 16.sp,
+                    maxLines = 1
+                )
+            }
+            val song by PlayManager.changeMusicLiveData().observeAsState()
             Icon(
-                ImageVector.vectorResource(id = playPauseState), null,
+                Icons.Rounded.MoreVert, null,
                 Modifier
-                    .size(middleButtonSize)
-                    .clip(RoundedCornerShape(middleButtonSize))
+                    .size(32.dp)
+                    .clip(RoundedCornerShape(32.dp))
+                    .background(translucentWhiteFixBug)
                     .clickable {
-                        if (PlayManager.isPaused())
-                            PlayManager.play()
-                        else PlayManager.pause()
+                        dialogNavController.navigate(DialogDestination.SongMenu(song!!))
                     },
                 tint = Color.White
             )
         }
-        Icon(
-            ImageVector.vectorResource(id = R.drawable.ic_skip_next), null,
-            Modifier
-                .size(middleButtonSize)
-                .padding(smallButtonPadding)
-                .clip(RoundedCornerShape(middleButtonSize))
-                .clickable {
-                    PlayManager.skipToNext()
-                },
-            tint = Color.White
+        //-----
+        fun Int.toTime(): String {
+            val s: Int = this / 1000
+            return (s / 60).toString() + ":" + s % 60
+        }
+
+        val musicPlaybackProgress by PlayManager.playingMusicProgressLiveData().map {
+            it
+        }.observeAsState(0)
+        val musicDuration by PlayManager.playingMusicDurationLiveData().map {
+            it
+        }.observeAsState(1)
+
+        SeekBar(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = musicPlaybackProgress,
+            maxValue = musicDuration,
+            onValueChange = { PlayManager.seekTo(it) }
         )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = musicPlaybackProgress.toTime(), color = translucentWhite)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = musicDuration.toTime(), color = translucentWhite)
+        }
+        //-----
+        val smallButtonPadding = 10.dp
+        val middleButtonSize = 80.dp
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            val playPauseState by PlayManager.pauseLiveData().map {
+                if (it) R.drawable.ic_play else R.drawable.ic_pause
+            }.observeAsState(R.drawable.ic_play)
+            Icon(
+                ImageVector.vectorResource(id = R.drawable.ic_skip_previous), null,
+                Modifier
+                    .size(middleButtonSize)
+                    .padding(smallButtonPadding)
+                    .clip(RoundedCornerShape(middleButtonSize))
+                    .clickable {
+                        PlayManager.skipToPrevious()
+                    },
+                tint = Color.White
+            )
+            Box(modifier = Modifier.padding(horizontal = 18.dp)) {
+                Icon(
+                    ImageVector.vectorResource(id = playPauseState), null,
+                    Modifier
+                        .size(middleButtonSize)
+                        .clip(RoundedCornerShape(middleButtonSize))
+                        .clickable {
+                            if (PlayManager.isPaused())
+                                PlayManager.play()
+                            else PlayManager.pause()
+                        },
+                    tint = Color.White
+                )
+            }
+            Icon(
+                ImageVector.vectorResource(id = R.drawable.ic_skip_next), null,
+                Modifier
+                    .size(middleButtonSize)
+                    .padding(smallButtonPadding)
+                    .clip(RoundedCornerShape(middleButtonSize))
+                    .clickable {
+                        PlayManager.skipToNext()
+                    },
+                tint = Color.White
+            )
+        }
     }
 }
+
+
